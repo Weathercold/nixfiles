81465c233397a643eaf3f534ef3fb2c9d7d831e2
Weathercold <weathercold.scr@gmail.com>
2022-11-26 14:52:44 -0500

    colloid/firefox: fix findbar padding

--- home/themes/base/firefox.nix
+++ home/themes/base/firefox.nix
@@ -1,21 +1,10 @@
 { config, lib, firefox-vertical-tabs, ... }:
 
-let
-  inherit (lib) mkMerge mkBefore;
-  cfg = config.nixfiles.programs.firefox;
-in
+let cfg = config.nixfiles.programs.firefox; in
 
 {
   imports = [ ../../programs/firefox.nix ];
 
-  programs.firefox.profiles.${cfg.profile}.userChrome = mkMerge [
-    (mkBefore ''@import "${firefox-vertical-tabs}/userChrome.css";'')
-    ''
-
-      /* Fix placement of window decorations */
-      * {
-        --uc-win-ctrl-vertical-offset: 0;
-      }
-    ''
-  ];
+  programs.firefox.profiles.${cfg.profile}.userChrome =
+    lib.mkBefore ''@import "${firefox-vertical-tabs}/userChrome.css";'';
 }
--- home/themes/colloid/firefox.nix
+++ home/themes/colloid/firefox.nix
@@ -1,7 +1,7 @@
 { config, lib, colloid-gtk-theme, ... }:
 
 let
-  inherit (lib) mkBefore;
+  inherit (lib) mkMerge mkBefore;
   cfg = config.nixfiles.programs.firefox;
 
   tme = colloid-gtk-theme + "/src/other/firefox/chrome/Colloid";
@@ -14,7 +14,21 @@ in
   ];
 
   programs.firefox.profiles.${cfg.profile} = {
-    userChrome = mkBefore ''@import "${tme}/theme.css";'';
+    userChrome = mkMerge [
+      (mkBefore ''@import "${tme}/theme.css";'')
+      ''
+
+        /* Fix placement of window decorations */
+        * {
+          --uc-win-ctrl-vertical-offset: 0;
+        }
+
+        /* Fix findbar padding */
+        .findbar-container {
+          padding: 25px !important;
+        }
+      ''
+    ];
     userContent = mkBefore ''
       @import "${tme}/colors/light.css";
       @import "${tme}/colors/dark.css";
@@ -27,6 +41,7 @@ in
     settings = {
       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       "browser.uidensity" = 2;
+      "svg.context-properties.content.enabled" = true;
       "browser.tabs.inTitlebar" = 1;
     };
   };
