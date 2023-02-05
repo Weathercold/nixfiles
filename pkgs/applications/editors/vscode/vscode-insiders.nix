3c18148a9c406b062e1bae5ea8444ac3f26e44ab
Weathercold <weathercold.scr@gmail.com>
2023-02-05 14:09:47 -0500

    vscode-insiders: remove posix_spawn fix
    The fix didn't work. The file permission is already preserved.

--- pkgs/applications/editors/vscode/vscode-insiders.nix
+++ pkgs/applications/editors/vscode/vscode-insiders.nix
@@ -27,54 +27,47 @@ let
     aarch64-darwin = "1va6bxigjwl5brxq36gpx2s3b0asd0ap3wnfxqg8i1nx0fnxzb9n";
     armv7l-linux = "1ccqr27mar1fzinw0skacwklyr839jz6z3ygk11h25j1b6763i7d";
   }.${system} or throwSystem;
-
-  pkg = callPackage "${path}/pkgs/applications/editors/vscode/generic.nix" rec {
-    version = "1.76.0-insider";
-    pname = "vscode-insiders";
-    updateScript = ./update-vscode-insiders.sh;
-
-    executableName = "code-insiders";
-    longName = "Visual Studio Code - Insiders";
-    shortName = "Code - Insiders";
-    inherit commandLineArgs;
-
-    src = fetchurl {
-      name = "VSCode_${version}_${plat}.${archive_fmt}";
-      url = "https://update.code.visualstudio.com/${version}/${plat}/insider";
-      inherit sha256;
-    };
-    sourceRoot = "";
-
-    meta = with lib; {
-      description = ''
-        Open source source code editor developed by Microsoft for Windows,
-        Linux and macOS
-      '';
-      mainProgram = "code-insiders";
-      longDescription = ''
-        Open source source code editor developed by Microsoft for Windows,
-        Linux and macOS. It includes support for debugging, embedded Git
-        control, syntax highlighting, intelligent code completion, snippets,
-        and code refactoring. It is also customizable, so users can change the
-        editor's theme, keyboard shortcuts, and preferences
-      '';
-      homepage = "https://code.visualstudio.com/";
-      downloadPage = "https://code.visualstudio.com/Updates";
-      license = licenses.unfree;
-      platforms = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-        "aarch64-linux"
-        "armv7l-linux"
-      ];
-    };
-  };
 in
 
-# Fix terminal and tasks not spawning
-pkg.overrideAttrs (prev: {
-  postInstall = prev.postInstall or "" + ''
-    chmod +x resources/app/node_modules.asar.unpacked/node-pty/build/Release/spawn-helper
-  '';
-})
+callPackage "${path}/pkgs/applications/editors/vscode/generic.nix" rec {
+  version = "1.76.0-insider";
+  pname = "vscode-insiders";
+  updateScript = ./update-vscode-insiders.sh;
+
+  executableName = "code-insiders";
+  longName = "Visual Studio Code - Insiders";
+  shortName = "Code - Insiders";
+  inherit commandLineArgs;
+
+  src = fetchurl {
+    name = "VSCode_${version}_${plat}.${archive_fmt}";
+    url = "https://update.code.visualstudio.com/${version}/${plat}/insider";
+    inherit sha256;
+  };
+  sourceRoot = "";
+
+  meta = with lib; {
+    description = ''
+      Open source source code editor developed by Microsoft for Windows,
+      Linux and macOS
+    '';
+    mainProgram = "code-insiders";
+    longDescription = ''
+      Open source source code editor developed by Microsoft for Windows,
+      Linux and macOS. It includes support for debugging, embedded Git
+      control, syntax highlighting, intelligent code completion, snippets,
+      and code refactoring. It is also customizable, so users can change the
+      editor's theme, keyboard shortcuts, and preferences
+    '';
+    homepage = "https://code.visualstudio.com/";
+    downloadPage = "https://code.visualstudio.com/Updates";
+    license = licenses.unfree;
+    platforms = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "aarch64-linux"
+      "armv7l-linux"
+    ];
+  };
+}
