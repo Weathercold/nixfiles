a317b004acb2b59431ee994e02c89b883d278675
Weathercold <weathercold.scr@gmail.com>
2023-09-16 19:43:30 -0400

    pkgs: migrate to by-name

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,22 +9,22 @@ let
 in
 
 rec {
-  anki-qt6 = callPackage ./games/anki/anki-qt6.nix { };
-  anki-bin-qt6 = callPackage ./games/anki/anki-bin-qt6.nix { };
+  anki-qt6 = callPackage ./an/anki/anki-qt6.nix { };
+  anki-bin-qt6 = callPackage ./an/anki/anki-bin-qt6.nix { };
 
-  catppuccin-discord = callPackage ./data/themes/catppuccin-discord/git.nix { };
+  catppuccin-discord = callPackage ./ca/catppuccin-discord/git.nix { };
 
-  colloid-gtk-theme-git = callPackage ./data/themes/colloid-gtk-theme/git.nix { };
+  colloid-gtk-theme-git = callPackage ./co/colloid-gtk-theme/git.nix { };
 
-  plymouth-themes = callPackage ./data/themes/plymouth-themes { };
+  plymouth-themes = callPackage ./pl/plymouth-themes { };
 
-  vscode-insiders = callPackage ./applications/editors/vscode/vscode-insiders.nix { };
+  vscode-insiders = callPackage ./vs/vscode/vscode-insiders.nix { };
   vscode-insiders-with-extensions =
     pkgs.vscode-with-extensions.override { vscode = vscode-insiders; };
 
-  vscodium-insiders = callPackage ./applications/editors/vscode/vscodium-insiders.nix { };
+  vscodium-insiders = callPackage ./vs/vscode/vscodium-insiders.nix { };
 
-  v2ray-rules-dat = callPackage ./data/misc/v2ray-rules-dat/bin.nix { };
+  v2ray-rules-dat = callPackage ./v2/v2ray-rules-dat/bin.nix { };
 
-  win2xcur = callPackage ./tools/misc/win2xcur { };
+  win2xcur = callPackage ./wi/win2xcur { };
 }
