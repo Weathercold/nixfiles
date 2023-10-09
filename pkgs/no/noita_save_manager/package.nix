f638097862ec3eabd8f9740c5164a190071478f3
Weathercold <weathercold.scr@gmail.com>
2023-10-08 21:18:07 -0400

    noita_save_manager: change game data and backup pathes

--- pkgs/no/noita_save_manager/package.nix
+++ pkgs/no/noita_save_manager/package.nix
@@ -1,6 +1,7 @@
 { lib
 , python3
 , fetchFromGitHub
+, makeWrapper
 }:
 
 python3.pkgs.buildPythonApplication rec {
@@ -17,12 +18,27 @@ python3.pkgs.buildPythonApplication rec {
 
   nativeBuildInputs = [
     python3.pkgs.poetry-core
+    makeWrapper
   ];
 
   propagatedBuildInputs = with python3.pkgs; [
     psutil
     pysimplegui
   ];
+
+  postInstall = ''
+    # Replace hardcoded path to Noita savegame directory under Proton
+    SAVE_MANAGER="$(find "$out" -name save_manager.py)"
+    substituteInPlace "$SAVE_MANAGER" \
+      --replace '"/home/j/gits/save_noita"' \
+                'os.path.expanduser("~") + "/.steam/steam/steamapps/compatdata/881100/pfx/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita"'
+
+    # Change backup location
+    wrapProgram "$out/bin/noita_save_manager" \
+      --run 'DIR="''${XDG_DATA_HOME:-~/.local/share}"
+             mkdir -p "$DIR/noita_save_manager"
+             cd "$DIR/noita_save_manager"'
+  '';
 
   pythonImportsCheck = [ "noita_save_manager" ];
 
